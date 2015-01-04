def1f7c9e412ec9cd4a5da9f3d0b019fc2f76666
Reuben John <reubenvjohn@gmail.com>
2015-01-04 17:59:33 +0300

    Removed external dependencies + FirstRunManager now listens for successful first run and follows method chain approach

--- FirstRunManager.java
+++ FirstRunManager.java
@@ -1,7 +1,9 @@
 package com.aspirephile.shared;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 
@@ -23,50 +25,79 @@ public class FirstRunManager {
 		}
 	};
 
-	private Context ctx;
+	private Activity activity;
+
 	private boolean isFirstRun;
 	private boolean firstRunChecked = false;
+	private String booleanKey;
+	private String sharedPrefsName;
 	private Class<?> firstRunClass;
 	private Class<?> notFirstRunClass;
+	SharedPreferences prefs;
 
 	public FirstRunManager(FragmentActivity activity) {
-		ctx = activity;
+		this.activity = activity;
+		prefs = this.activity.getSharedPreferences(sharedPrefsName,
+				Context.MODE_PRIVATE);
 	}
 
 	public boolean isFirstRun() {
 		if (!firstRunChecked) {
-			isFirstRun = ctx.getSharedPreferences(
-					EventBookerProps.files.appPrefs, Context.MODE_PRIVATE)
-					.getBoolean(EventBookerProps.keys.prefs.firstRun,
-							defaults.isfirstRun);
+			isFirstRun = prefs.getBoolean(booleanKey, defaults.isfirstRun);
 			firstRunChecked = true;
 		}
 		return isFirstRun;
 	}
 
-	public void setOnFirstRunClass(Class<?> cls) {
+	public FirstRunManager setOnFirstRunClass(Class<?> cls) {
 		this.firstRunClass = cls;
+		return this;
 	}
 
-	public void setOnNotFirstRunClass(Class<?> cls) {
+	public FirstRunManager setOnNotFirstRunClass(Class<?> cls) {
 		this.notFirstRunClass = cls;
+		return this;
 	}
 
-	public void launchNextActivity() {
+	public FirstRunManager setSharedPrefsName(String sharedPrefsName) {
+		this.sharedPrefsName = sharedPrefsName;
+		return this;
+	}
+
+	public FirstRunManager setBooleanKey(String booleanKey) {
+		this.booleanKey = booleanKey;
+		return this;
+	}
+
+	public FirstRunManager launchNextActivity() {
 		Class<?> cls;
-		if (isFirstRun())
+		if (isFirstRun()) {
+			l.i("Initiating first run protocol");
 			cls = firstRunClass;
-		else
+		} else {
+
+			l.i("Previous run detected. Skipping first run protocol");
 			cls = notFirstRunClass;
-		Intent i = new Intent(ctx, cls);
-		l.d(ctx + " launching next activity: " + cls + "(" + i + ")");
-		ctx.startActivity(i);
+		}
+		Intent i = new Intent(activity, cls);
+		l.d(activity + " launching next activity: " + cls + "(" + i + ")");
+		activity.startActivityForResult(i, EventBookerProps.codes.welcome);
+		return this;
 	}
 
-	public void scheduleNextActivity(int splashscreenduration) {
+	public FirstRunManager scheduleNextActivity(int splashscreenduration) {
 		l.d("Scheduling next activity launch for " + splashscreenduration
 				+ "ms delay");
 		new Handler().postDelayed(launchNextActivity, splashscreenduration);
+		return this;
+	}
+
+	public void sendFirstRunCompletionResult(boolean isFirstRunSuccessful) {
+		l.d("Received message: First run successful: " + isFirstRunSuccessful);
+		if (isFirstRunSuccessful) {
+			l.d("Marking indicator of successful first run");
+			prefs.edit().putBoolean(booleanKey, !isFirstRunSuccessful).commit();
+		}
 	}
 
 }
