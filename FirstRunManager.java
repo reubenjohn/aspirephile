c99b299820c610f59395fe6c5e8f5e025656e7a9
Reuben John <reubenvjohn@gmail.com>
2015-01-18 19:22:55 +0530

    Made request code a configurable launch configuration

--- FirstRunManager.java
+++ FirstRunManager.java
@@ -8,7 +8,6 @@ import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 
 import com.aspirephile.shared.debug.Logger;
-import com.funtainment.eventbooker.EventBookerProps;
 
 public class FirstRunManager {
 	Logger l = new Logger(FirstRunManager.class);
@@ -30,27 +29,64 @@ public class FirstRunManager {
 	private String booleanKey;
 	private String sharedPrefsName;
 
-	private class RunConfig {
+	public class RunConfig {
 		public Class<?> launchActivity;
+		public int requestCode;
 		public int enterAnim;
 		public int exitAnim;
 
 		public RunConfig() {
 			launchActivity = null;
+			requestCode = 0;
 			enterAnim = 0;
 			exitAnim = 0;
 		}
 
 		public boolean isLaunchConfigurationValid() {
-			return ((launchActivity != null));
+			return ((launchActivity != null) && (requestCode != 0));
 		}
 
 		public boolean isAnimationConfigurationValid() {
 			return (enterAnim > 0) && (exitAnim > 0);
 		}
+
+		public RunConfig setLaunchActivity(Class<?> cls) {
+			launchActivity = cls;
+			return RunConfig.this;
+		}
+
+		public RunConfig setRequestCode(int requestCode) {
+			this.requestCode = requestCode;
+			return RunConfig.this;
+		}
+		public RunConfig setTransitions(int enterAnim, int exitAnim) {
+			this.enterAnim = enterAnim;
+			this.exitAnim = exitAnim;
+			return RunConfig.this;
+		}
+
+		@Override
+		public String toString() {
+			return "RunConfig("
+					+ (isLaunchConfigurationValid() == true ? "Valid"
+							: "Invalid")
+					+ "){ "
+					+ "Launch activity: "
+					+ launchActivity.getName()
+					+ ", "
+					+ "request code: "
+					+ requestCode
+					+ ", "
+					+ "Animations("
+					+ (isAnimationConfigurationValid() == true ? "Valid"
+							: "Invalid") + "){ " + "Enter: " + enterAnim + ", "
+					+ "Exit: " + exitAnim + " } }";
+		}
+
+
 	}
 
-	RunConfig firstRunConfigs, subsequentRunConfigs;
+	public RunConfig firstRunConfig, subsequentRunConfig;
 
 	SharedPreferences prefs;
 
@@ -58,35 +94,12 @@ public class FirstRunManager {
 		this.activity = activity;
 		prefs = this.activity.getSharedPreferences(sharedPrefsName,
 				Context.MODE_PRIVATE);
-		firstRunConfigs = new RunConfig();
-		subsequentRunConfigs = new RunConfig();
+		firstRunConfig = new RunConfig();
+		subsequentRunConfig = new RunConfig();
 	}
 
 	public boolean isFirstRun() {
 		return prefs.getBoolean(booleanKey, defaults.isfirstRun);
-	}
-
-	public FirstRunManager setOnFirstRunClass(Class<?> cls) {
-		firstRunConfigs.launchActivity = cls;
-		return this;
-	}
-
-	public FirstRunManager setOnSubsequentRunClass(Class<?> cls) {
-		subsequentRunConfigs.launchActivity = cls;
-		return this;
-	}
-
-	public FirstRunManager setFirstRunTransition(int enterAnim, int exitAnim) {
-		firstRunConfigs.enterAnim = enterAnim;
-		firstRunConfigs.exitAnim = exitAnim;
-		return FirstRunManager.this;
-	}
-
-	public FirstRunManager setSubsequentRunTransition(int enterAnim,
-			int exitAnim) {
-		subsequentRunConfigs.enterAnim = enterAnim;
-		subsequentRunConfigs.exitAnim = exitAnim;
-		return FirstRunManager.this;
 	}
 
 	public FirstRunManager setSharedPrefsName(String sharedPrefsName) {
@@ -100,29 +113,37 @@ public class FirstRunManager {
 	}
 
 	public FirstRunManager launchNextActivity() {
-		RunConfig runConfigs;
+		RunConfig runConfig;
 		boolean isFirstRun = isFirstRun();
 		// Setup appropriate launch configurations
 		if (isFirstRun) {
 			l.i("Initiating first run protocol");
-			runConfigs = firstRunConfigs;
+			runConfig = firstRunConfig;
 		} else {
 
 			l.i("Previous run detected. Skipping first run protocol");
-			runConfigs = subsequentRunConfigs;
+			runConfig = subsequentRunConfig;
 		}
 		// Validate and start activity
-		if (runConfigs.isLaunchConfigurationValid()) {
-			Intent i = new Intent(activity, runConfigs.launchActivity);
+		if (runConfig.isLaunchConfigurationValid()) {
+			Intent i = new Intent(activity, runConfig.launchActivity);
 			l.d(activity + " launching next activity: "
-					+ runConfigs.launchActivity + "(" + i + ")");
-			activity.startActivityForResult(i, EventBookerProps.codes.welcome);
+					+ runConfig.launchActivity + "(" + i + ")");
+			activity.startActivityForResult(i, runConfig.requestCode);
+		} else {
+			l.e("Invalid run configurations: " + runConfig);
+			// Validate and override pending transition
+			if (runConfig.isAnimationConfigurationValid()) {
+				activity.overridePendingTransition(runConfig.enterAnim,
+						runConfig.exitAnim);
+			} else
+				l.w("Invalid animation configurations: " + runConfig);
+			// TODO Throw appropriate exceptions describing error (maybe with
+			// launch
+			// configurations)
 		}
-		// Validate and override pending transition
-		if (runConfigs.isAnimationConfigurationValid()) {
-			activity.overridePendingTransition(runConfigs.enterAnim,
-					runConfigs.exitAnim);
-		}
+		// TODO Throw appropriate exceptions describing error (maybe with launch
+		// configurations)
 
 		// Lastly finish root activity to prevent return to it on back button
 		// press from the subsequent activity
