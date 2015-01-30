7c298fb93847060b4c90057881aaed8800df914d
Reuben John <reubenvjohn@gmail.com>
2015-01-30 23:05:02 +0530

    Added NullPointerException constructor with logger as a parameter

--- debug/NullPointerAsserter.java
+++ debug/NullPointerAsserter.java
@@ -4,67 +4,75 @@ import android.util.Log;
 
 public class NullPointerAsserter {
 
-	String tag;
+    Logger l;
 
-	public NullPointerAsserter(String tag) {
-		this.tag = tag;
-	}
+    public NullPointerAsserter(String tag) {
+        l = new Logger(tag);
+    }
 
-	public NullPointerAsserter(Class<?> cls) {
-		this.tag = cls.getCanonicalName();
-	}
+    public NullPointerAsserter(Class<?> cls) {
+        l = new Logger(cls.getCanonicalName());
+    }
 
-	public boolean assertPointer(Object... objects) {
-		boolean allGood = true;
-		try {
-			boolean array = objects.length > 1; // Also checks if objects points
-												// to null
-			for (int i = 0; i < objects.length; i++) {
-				try {
-					objects[i].getClass();
-				} catch (NullPointerException e) {
-					allGood = false;
-					Log.e(tag, "NullPointerException of object"
-							+ (array ? ("[" + i + "]") : ""));
-					removeFromStackBottom(e);
-					e.printStackTrace();
-				}
-			}
-		} catch (NullPointerException e) {
-			allGood = false;
-			Log.e(tag, "NullPointerException of object");
-			removeFromStackBottom(e);
-			e.printStackTrace();
-		}
+    public NullPointerAsserter(Logger l) {
+        this.l = l;
+    }
 
-		return allGood;
-	}
+    public boolean assertPointer(Object... objects) {
+        boolean allGood = true;
+        try {
+            boolean array = objects.length > 1; // Also checks if objects points
+            // to null
+            for (int i = 0; i < objects.length; i++) {
+                try {
+                    objects[i].getClass();
+                } catch (NullPointerException e) {
+                    allGood = false;
+                    l.e("NullPointerException of object"
+                            + (array ? ("[" + i + "]") : ""));
+                    removeFromStackBottom(e);
+                    e.printStackTrace();
+                }
+            }
+        } catch (NullPointerException e) {
+            allGood = false;
+            l.e("NullPointerException of object");
+            removeFromStackBottom(e);
+            e.printStackTrace();
+        }
 
-	public boolean assertPointerQuietly(Object... objects) {
-		if (objects != null) {
-			for (int i = 0; i < objects.length; i++) {
-				if (objects[i] == null) {
-					return false;
-				}
-			}
-		} else
-			return false;
-		return true;
-	}
+        return allGood;
+    }
 
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
+    public boolean assertPointerQuietly(Object... objects) {
+        if (objects != null) {
+            for (int i = 0; i < objects.length; i++) {
+                if (objects[i] == null) {
+                    return false;
+                }
+            }
+        } else
+            return false;
+        return true;
+    }
 
-	private void removeFromStackBottom(Exception e) {
-		if (e != null) {
-			StackTraceElement[] ea = e.getStackTrace();
-			if (ea != null) {
-				for (int j = 0; j < ea.length - 1; j++) {
-					ea[j] = ea[j + 1];
-				}
-				e.setStackTrace(ea);
-			}
-		}
-	}
+    public void setTag(String tag) {
+        l.setTag(tag);
+    }
+    public void setTag(Class<?> cls) {
+        l.setTag(cls);
+    }
+
+
+    private void removeFromStackBottom(Exception e) {
+        if (e != null) {
+            StackTraceElement[] ea = e.getStackTrace();
+            if (ea != null) {
+                for (int j = 0; j < ea.length - 1; j++) {
+                    ea[j] = ea[j + 1];
+                }
+                e.setStackTrace(ea);
+            }
+        }
+    }
 }
